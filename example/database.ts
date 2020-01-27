2271fc0ec299f6973033068dc6194ad93c99c387
Yurick <yurick.hauschild@gmail.com>
2020-01-27 22:30:50 +0100

    ✨ Add "database" to example

---
+++ example/database.ts
@@ -0,0 +1,89 @@
+// This is here for entertainment and for example purposes only
+// Please don't use this code for reference of anything in your life
+
+interface Order {
+  id: string
+  product: string
+  value: number
+}
+
+interface User {
+  name: string
+  phoneNumber: string
+  orders: Order[]
+}
+
+type Database = {
+  [id: string]: User
+}
+
+const DB = {} as Database
+
+export const getUser = async (id: string): Promise<User | undefined> => DB[id]
+
+const existsUser = async (id: string): Promise<boolean> =>
+  (await getUser(id)) === undefined
+
+export const updateUser = async (
+  id: string,
+  user: Partial<User>,
+): Promise<boolean> => {
+  if (!(await existsUser(id))) return false
+
+  DB[id] = {
+    ...DB[id],
+    ...user,
+  }
+  return true
+}
+
+export const deleteUser = async (id: string): Promise<boolean> => {
+  if (!(await existsUser(id))) return false
+
+  delete DB[id]
+  return true
+}
+
+export const getOrdersForUser = async (
+  userId: string,
+): Promise<Order[] | undefined> => DB[userId]?.orders
+
+export const updateOrderForUser = async (
+  userId: string,
+  orderId: string,
+  order: Partial<Order>,
+): Promise<boolean> => {
+  if (!existsUser(userId)) return false
+  const orders = await getOrdersForUser(userId)
+
+  const previousOrderIndex = orders.findIndex(order => order.id === orderId)
+
+  if (previousOrderIndex === -1) return false
+
+  orders[previousOrderIndex] = {
+    ...orders[previousOrderIndex],
+    ...order,
+  }
+
+  return true
+}
+
+export const deleteOrderForUser = async (
+  userId: string,
+  orderId: string,
+): Promise<boolean> => {
+  if (!existsUser(userId)) return false
+  const orders = await getOrdersForUser(userId)
+
+  const orderIndex = orders.findIndex(order => order.id === orderId)
+
+  if (orderIndex === -1) return false
+
+  orders.splice(orderIndex, 1)
+  return true
+}
+
+export const authenticate = async (
+  email: string,
+  password: string,
+): Promise<boolean> => email === 'admin' && password === 'admin'
