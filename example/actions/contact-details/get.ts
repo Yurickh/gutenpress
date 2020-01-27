29d89b204091907aee8e0cf12b8a0a89ffad5adb
Yurick <yurick.hauschild@gmail.com>
2020-01-28 00:24:32 +0100

    ✨ Add getContactDetails endpoint

--- example/actions/contact-details/get.ts
+++ example/actions/contact-details/get.ts
@@ -1,1 +1,15 @@
-export const getContactDetails = (): boolean => true
+import { Action } from 'gutenpress'
+import { Token } from 'example/wrappers/authenticate'
+import { getUser } from 'example/database'
+
+export const getContactDetails: Action<
+  { name: string; phoneNumber: string },
+  { context: Token }
+> = async ({ context: token }) => {
+  const user = await getUser(token.userId)
+
+  return {
+    name: user.name,
+    phoneNumber: user.phoneNumber,
+  }
+}
