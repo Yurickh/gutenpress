46d239a40d92876644337cc894de52c0c982875c
Yurick <yurick.hauschild@gmail.com>
2020-03-01 00:49:22 +0100

    📄 Update example with proper authentication function

--- example/wrappers/authenticate.ts
+++ example/wrappers/authenticate.ts
@@ -1,5 +1,22 @@
+import { UnauthorizedError } from 'gutenpress'
+import { authenticate as authenticateInDatabase } from '../database'
+
 export type Token = {
   userId: string
 }
 
-export const authenticate = (): boolean => true
+export const authenticate = (
+  _context: {},
+  request: Request,
+): Token | UnauthorizedError => {
+  const [_bearer, token] = request.headers.get('authorization').split(' ')
+  const [username, password] = atob(token).split(':')
+
+  if (authenticateInDatabase(username, password)) {
+    return {
+      userId: username,
+    }
+  } else {
+    return new UnauthorizedError('Invalid token')
+  }
+}
