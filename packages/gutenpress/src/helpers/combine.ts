f7e1664ee05a11fa4a272f6bb06b6d25025f78cd
Yurick <yurick.hauschild@gmail.com>
2020-03-27 14:30:57 -0300

    🐛 Change combine so methods on the same path merge together

--- packages/gutenpress/src/helpers/combine.ts
+++ packages/gutenpress/src/helpers/combine.ts
@@ -1,18 +1,33 @@
-import { Resource, KeysOf } from '../types'
+import { Resource, KeysOf, MethodGroup } from '../types'
+import { fromEntries } from './fromEntries'
 
 export const combine = <
   Context,
   Resources extends Resource<Path, Context>[],
-  Path extends string = any
+  Path extends string = any,
+  ResourcePath extends string = Resources extends (infer R)[]
+    ? KeysOf<R>
+    : never
 >(
   resources: Resources,
-): Resource<Resources extends (infer R)[] ? KeysOf<R> : never, Context> => {
-  // TODO: merge methods that belong to the same path together
+) => {
   return resources.reduce(
     (acc, curr) => ({
-      ...acc,
+      // add any new paths
       ...curr,
+      // and extends existing paths with the new methods
+      ...fromEntries<Resource<ResourcePath, Context>>(
+        Object.entries<MethodGroup<Context>>(acc).map(([path, methodGroup]) => [
+          path as ResourcePath,
+          {
+            ...methodGroup,
+            // NOTE: this is kind of a hack, as `path` is not necessarily present in `curr`
+            // but spreading undefined is a no-op so we're virtually safe in this operation.
+            ...curr[path as keyof typeof curr],
+          },
+        ]),
+      ),
     }),
-    {},
+    {} as Resource<ResourcePath, Context>,
   )
 }
