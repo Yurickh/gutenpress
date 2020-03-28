3e695fb83b683166e6c6bf82f10e3c898ecee7f9
Yurick <yurick.hauschild@gmail.com>
2020-03-28 09:32:56 -0300

    🔥 Remove `toRouter` from gutenpress' api

--- packages/gutenpress/src/index.ts
+++ packages/gutenpress/src/index.ts
@@ -1,7 +1,6 @@
 export { get, post, put } from './methods'
 export * from './errors'
 export { wrap } from './wrap'
-export { toRouter } from './toRouter'
 export { combine } from './helpers/combine'
 
 export { RequestParams, Resource, HTTPMethod } from './types'
