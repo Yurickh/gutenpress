5862581137142ff0c0a427ee4314184c04182e2b
Yurick <yurick.hauschild@gmail.com>
2020-05-02 20:01:50 +0200

    ✨ Make wrap types more prone to be autocompletion

--- packages/gutenpress/core/src/wrap.ts
+++ packages/gutenpress/core/src/wrap.ts
@@ -1,48 +1,112 @@
 import { mapObject } from '@gutenpress/helpers'
-import {
-  Resource,
-  KeysOf,
-  HTTPMethod,
-  RequestParams,
-  MethodGroup,
-} from './types'
+import { Resource, HTTPMethod, RequestParams, MethodGroup } from './types'
 import { combine } from './helpers/combine'
 
 type NonErrorReturn<T> = T extends Error ? never : T
+type ErrorReturn<T> = T extends Error ? T : never
 
 type Wrapper<OutputContext, InputContext = {}> = (
   params: RequestParams<InputContext, HTTPMethod>,
 ) => OutputContext
 
-const wrapMethods = <OutputContext, InputContext>(
+type WrappedAction<
+  OutputContext,
+  InputContext,
+  Method extends HTTPMethod,
+  Action extends (
+    params: RequestParams<NonErrorReturn<OutputContext>, any>,
+  ) => any
+> = (
+  params: RequestParams<InputContext, Method>,
+) =>
+  | ErrorReturn<OutputContext>
+  | (Action extends (
+      params: RequestParams<NonErrorReturn<OutputContext>, Method>,
+    ) => infer Result
+      ? ErrorReturn<OutputContext> | Result
+      : never)
+
+const wrapAction = <
+  OutputContext,
+  InputContext,
+  Action extends (
+    params: RequestParams<NonErrorReturn<OutputContext>, any>,
+  ) => any
+>(
+  action: Action,
   wrapper: Wrapper<OutputContext, InputContext>,
-  single: MethodGroup<NonErrorReturn<OutputContext>>,
-): MethodGroup<InputContext> =>
-  mapObject(
-    ([methodName, action]) => [
-      methodName,
-      (params: RequestParams<InputContext, typeof methodName>) => {
-        const outputContext = wrapper(params)
+): WrappedAction<
+  OutputContext,
+  InputContext,
+  Action extends (p: RequestParams<any, infer M>) => any ? M : never,
+  Action
+> => (params) => {
+  const outputContext = wrapper(params) as
+    | NonErrorReturn<OutputContext>
+    | ErrorReturn<OutputContext>
 
-        if (outputContext instanceof Error) {
-          return outputContext
-        }
+  if (outputContext instanceof Error) {
+    return outputContext
+  }
 
-        return action?.({
-          ...params,
-          context: outputContext as NonErrorReturn<OutputContext>,
-        })
-      },
-    ],
-    single,
-  )
+  return action?.({
+    ...params,
+    context: outputContext as NonErrorReturn<OutputContext>,
+  })
+}
 
-const wrapResource = <InputContext, OutputContext>(
+type WrapMethods<
+  MG extends MethodGroup<OutputContext>,
+  OutputContext,
+  InputContext
+> = {
+  [method in keyof MG]: method extends HTTPMethod
+    ? MG[method] extends (p: RequestParams<any, any>) => infer Result
+      ? (p: RequestParams<InputContext, method>) => Result
+      : never
+    : never
+}
+
+const wrapMethods = <
+  OutputContext,
+  InputContext,
+  MG extends MethodGroup<NonErrorReturn<OutputContext>>
+>(
   wrapper: Wrapper<OutputContext, InputContext>,
-): (<Path extends string>(
-  resource: Resource<Path, NonErrorReturn<OutputContext>>,
-) => Resource<Path, InputContext>) =>
-  mapObject(([path, methods]) => [path, wrapMethods(wrapper, methods)])
+  single: MG,
+): WrapMethods<MG, NonErrorReturn<OutputContext>, InputContext> =>
+  mapObject(([method, action]) => {
+    if (action === undefined) return [method, undefined]
+
+    // NOTE: not proud of this `action as any` here.
+    // It seems ts can't open `MG[keyof MG]` since it doesn't know
+    // exactly what's the type of it, but that's precisely what we want :T
+    // Feel free to open a PR if you know how to solve this.
+    return [method, wrapAction(action as any, wrapper)]
+  }, single) as WrapMethods<MG, NonErrorReturn<OutputContext>, InputContext>
+
+type WrappedResource<
+  OutputContext,
+  InputContext,
+  R extends Resource<string, NonErrorReturn<OutputContext>>
+> = {
+  [path in keyof R]: WrapMethods<
+    R[path],
+    NonErrorReturn<OutputContext>,
+    InputContext
+  >
+}
+
+const wrapResource = <OutputContext, InputContext>(
+  wrapper: Wrapper<OutputContext, InputContext>,
+): (<R extends Resource<string, NonErrorReturn<OutputContext>>>(
+  resource: R,
+) => WrappedResource<OutputContext, InputContext, R>) =>
+  mapObject(([path, methods]) => [path, wrapMethods(wrapper, methods)]) as <
+    R extends Resource<string, NonErrorReturn<OutputContext>>
+  >(
+    resource: R,
+  ) => WrappedResource<OutputContext, InputContext, R>
 
 export const wrap = <
   Resources extends Resource<any, NonErrorReturn<OutputContext>>[],
@@ -51,11 +115,4 @@ export const wrap = <
 >(
   wrapper: Wrapper<OutputContext, InputContext>,
   resources: Resources,
-): Resource<
-  Resources extends (infer R)[] ? KeysOf<R> : never,
-  InputContext
-> => {
-  const mappedResources = resources.map(wrapResource(wrapper))
-
-  return combine(mappedResources)
-}
+) => wrapResource(wrapper)(combine(resources))
--- packages/gutenpress/core/type-tests/wrap.test-d.ts
+++ packages/gutenpress/core/type-tests/wrap.test-d.ts
@@ -34,3 +34,37 @@ import { get, wrap } from '../src'
     ]),
   )
 }
+
+/** Test: wrap works with autocompletion */
+// {
+//   const a = get('/a', ({ context }) => 'a' + context)
+//   const b = get('/b', ({ context }) =>
+//     context ? 13 : new BadRequestError('ih'),
+//   )
+//   const a2 = post('/a', () => 'a2')
+
+//   const wrapped = wrap(() => 'context', [a, b, a2])
+
+//   expectType<{
+//     '/a': {
+//       GET: (p: RequestParams<{}, 'GET'>) => string
+//       POST: (p: RequestParams<{}, 'POST'>) => string
+//     }
+//     '/b': { GET: (p: RequestParams<{}, 'GET'>) => number | BadRequestError }
+//   }>(wrapped)
+// }
+
+// /** Test: wrap properly handles the error type */
+// {
+//   const a = get('/a', ({ context }) => 'a' + context)
+//   const wrapper = ({ body }: { body: { token?: string } }) =>
+//     body.token ?? new BadRequestError('please provide token')
+
+//   const wrapped = wrap(wrapper, [a])
+
+//   expectType<{
+//     '/a': {
+//       GET: (p: RequestParams<{}, 'GET'>) => string | BadRequestError
+//     }
+//   }>(wrapped)
+// }
