77d751c6255cfdcf9f1d85d0f99b7125787e8544
Yurick <yurick.hauschild@gmail.com>
2020-05-02 11:36:21 +0200

    ✨ Expose UnionToIntersection in @g/helpers

--- packages/gutenpress/helpers/src/fromEntries/index.ts
+++ packages/gutenpress/helpers/src/fromEntries/index.ts
@@ -4,21 +4,13 @@
  * @see https://gist.github.com/karol-majewski/b234a4aceb8884ccc1acf25a2e1ed16e
  */
 
+import { UnionToIntersection } from '../UnionToIntersection'
+
 type Primitive = boolean | number | string | bigint | symbol | null | undefined
 
 type Narrowable = Primitive | object | {}
 
 type Entry<K extends PropertyKey, V> = [K, V]
-
-/**
- * @author https://stackoverflow.com/users/2887218/jcalz
- * @see https://stackoverflow.com/a/50375286/10325032
- */
-type UnionToIntersection<Union> = (
-  Union extends any ? (argument: Union) => void : never
-) extends (argument: infer Intersection) => void
-  ? Intersection
-  : never
 
 type FromEntries<
   T extends Entry<K, V>,
